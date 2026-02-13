23324501a7e66aeca8b32127d76b15d0246ce67b
Dušan Komadinović <dusan.komadinovic20@gmail.com>
2026-02-13 22:03:18 +0100

    Link grafana dashboard with angular

--- frontend/src/app/features/dashboard/dashboard.ts
+++ frontend/src/app/features/dashboard/dashboard.ts
@@ -1,11 +1,35 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {DomSanitizer, SafeResourceUrl} from '@angular/platform-browser';
+import {environment} from '../../../environments/environment';
+import {MatExpansionModule} from '@angular/material/expansion';
+import {MatButtonToggle, MatButtonToggleGroup} from '@angular/material/button-toggle';
+import {FormsModule} from '@angular/forms';
 
 @Component({
-  selector: 'app-dashboard',
-  imports: [],
-  templateUrl: './dashboard.html',
-  styleUrl: './dashboard.css',
+    selector: 'app-dashboard',
+    imports: [CommonModule, MatExpansionModule, MatButtonToggleGroup, MatButtonToggle, FormsModule],
+    templateUrl: './dashboard.html',
+    styleUrl: './dashboard.css',
 })
 export class Dashboard {
+    dashboards: { id: string; url: SafeResourceUrl }[];
+    selectedDashboardId = 'PI1';
 
+    constructor(private sanitizer: DomSanitizer) {
+        this.dashboards = [
+            {
+                id: 'PI1',
+                url: this.sanitizer.bypassSecurityTrustResourceUrl(environment.grafanaPi1DashboardUrl),
+            },
+            {
+                id: 'PI2',
+                url: this.sanitizer.bypassSecurityTrustResourceUrl("https://example.com/tmp1"),
+            },
+            {
+                id: 'PI3',
+                url: this.sanitizer.bypassSecurityTrustResourceUrl("https://example.com/tmp2"),
+            },
+        ];
+    }
 }
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
     production: true,
-    apiUrl: 'http://localhost:3000',
-    frontEntranceCameraStreamUrl: 'http://localhost:8080/?action=stream'
+    apiUrl: 'http://localhost:8000',
+    frontEntranceCameraStreamUrl: 'http://localhost:8080/?action=stream',
+    grafanaPi1DashboardUrl: 'http://localhost:3005/public-dashboards/2a1b760f0884459c99c1a5b841a7e0d3'
 };
